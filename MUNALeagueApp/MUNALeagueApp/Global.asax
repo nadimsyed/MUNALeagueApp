a3aa402bd0f4dfd28689ada2dd90a379c80fd08f
nadimsyed <syed.m.nadim@gmail.com>
2018-08-12 13:35:45 -0400

    first commit

---
+++ MUNALeagueApp/MUNALeagueApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MUNALeagueApp.MvcApplication" Language="C#" %>
---
+++ MUNALeagueApp/MUNALeagueApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MUNALeagueApp.Startup))]
+namespace MUNALeagueApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
